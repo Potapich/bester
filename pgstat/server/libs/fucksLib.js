1616be384c17f60472ac10e8256159ae4aca322a
Potapich <mihashadep@gmail.com>
2022-02-27 23:46:39 +0200

    RussianMilitaryShip-GoFuckYourself to db / 27.02.2022

--- pgstat/server/libs/fucksLib.js
+++ pgstat/server/libs/fucksLib.js
@@ -8,69 +8,10 @@ let dbo;
 let fucksBase;
 let arr;
 
-//add/edit new record to game list
-async function addFucksUrls(body) {
-    try {
-        arr = body
-    } catch (e) {
-        console.log('MONGO_ERROR', e);
-    }
-}
-
-async function updateFuckUrl(body) {
-    try {
-        const newArr = arr.map(obj => {
-            if (obj.id === 1) {
-                return {...obj, isLive: body.isLive};
-            }
-            return obj;
-        });
-        arr = newArr
-    } catch (e) {
-        console.log('MONGO_ERROR', e);
-    }
-}
-
-async function getFuckUrls() {
-    try {
-        return arr
-    } catch (e) {
-        console.log('MONGO_ERROR', e);
-    }
-}
-
-// (function mongo_starter() {
-//     MongoClient.connect(urlDB, {   // + '/' + dbName, {
-//         useUnifiedTopology: true, useNewUrlParser: true
-//     }, function (err, db) {
-//         if (err) {
-//             console.log(err);
-//             return err;
-//         } else {
-//             console.log("Connected successfully to db");
-//             dbo = db.db();
-//             fucksBase = dbo.collection(mongoConfig.fucksBase);
-//             fucksBase.createIndex({'url': 1});
-//
-//         }
-//     })
-// })();
-
 // //add/edit new record to game list
 // async function addFucksUrls(body) {
 //     try {
-//         for (let key in body) {
-//             let recordExists = await checkRecordExistence(body[key].url)
-//             if (recordExists) {
-//                 fucksBase.replaceOne({
-//                     url: body[key].url
-//                 }, function (err, result){
-//                     console.log('err: ', err, 'result: ', result)
-//                 });
-//             } else {
-//                 await fucksBase.insertOne(body);
-//             }
-//         }
+//         arr = body
 //     } catch (e) {
 //         console.log('MONGO_ERROR', e);
 //     }
@@ -78,16 +19,13 @@ async function getFuckUrls() {
 //
 // async function updateFuckUrl(body) {
 //     try {
-//         return await fucksBase.updateOne(
-//             { "url": body.url}, // Filter
-//             { "isLive": body.isLive} // Update
-//         )
-//             .then((obj) => {
-//                 console.log('Updated - ' + obj);
-//             })
-//             .catch((err) => {
-//                 console.log('Error: ' + err);
-//             })
+//         const newArr = arr.map(obj => {
+//             if (obj.id === 1) {
+//                 return {...obj, isLive: body.isLive};
+//             }
+//             return obj;
+//         });
+//         arr = newArr
 //     } catch (e) {
 //         console.log('MONGO_ERROR', e);
 //     }
@@ -95,21 +33,84 @@ async function getFuckUrls() {
 //
 // async function getFuckUrls() {
 //     try {
-//         return await fucksBase.find({'_id': -1}).toArray();
+//         return arr
 //     } catch (e) {
 //         console.log('MONGO_ERROR', e);
 //     }
 // }
-//
-// async function checkRecordExistence(url) {
-//     try {
-//         const dbRequest = await fucksBase.find({'url': url}).toArray();
-//         console.log('dbRequest: ', dbRequest)
-//         return Boolean(dbRequest.length);
-//     } catch (e) {
-//         console.log('MONGO_ERROR', e);
-//     }
-// }
+
+(function mongo_starter() {
+    MongoClient.connect(urlDB, {   // + '/' + dbName, {
+        useUnifiedTopology: true, useNewUrlParser: true
+    }, function (err, db) {
+        if (err) {
+            console.log(err);
+            return err;
+        } else {
+            console.log("Connected successfully to db");
+            dbo = db.db();
+            fucksBase = dbo.collection(mongoConfig.fucksBase);
+            fucksBase.createIndex({'url': 1});
+
+        }
+    })
+})();
+
+//add/edit new record to game list
+async function addFucksUrls(body) {
+    console.log(body)
+    try {
+        for (let key in body) {
+            let recordExists = await checkRecordExistence(body[key].url)
+            if (recordExists) {
+                fucksBase.replaceOne({
+                    url: body[key].url
+                }, function (err, result){
+                    console.log('err: ', err, 'result: ', result)
+                });
+            } else {
+                await fucksBase.insertOne(body);
+            }
+        }
+    } catch (e) {
+        console.log('MONGO_ERROR', e);
+    }
+}
+
+async function updateFuckUrl(body) {
+    try {
+        return await fucksBase.updateOne(
+            { "url": body.url}, // Filter
+            { "isLive": body.isLive} // Update
+        )
+            .then((obj) => {
+                console.log('Updated - ' + obj);
+            })
+            .catch((err) => {
+                console.log('Error: ' + err);
+            })
+    } catch (e) {
+        console.log('MONGO_ERROR', e);
+    }
+}
+
+async function getFuckUrls() {
+    try {
+        return await fucksBase.find({'_id': -1}).toArray();
+    } catch (e) {
+        console.log('MONGO_ERROR', e);
+    }
+}
+
+async function checkRecordExistence(url) {
+    try {
+        const dbRequest = await fucksBase.find({'url': url}).toArray();
+        console.log('dbRequest: ', dbRequest)
+        return Boolean(dbRequest.length);
+    } catch (e) {
+        console.log('MONGO_ERROR', e);
+    }
+}
 
 
 module.exports.addFuckUrls = addFucksUrls
