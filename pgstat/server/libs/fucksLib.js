dbd8bc521ce87dc216b20e88aed44416168081ec
Potapich <mihashadep@gmail.com>
2022-02-27 23:42:53 +0200

    RussianMilitaryShip-GoFuckYourself to temp / 27.02.2022

--- pgstat/server/libs/fucksLib.js
+++ pgstat/server/libs/fucksLib.js
@@ -6,6 +6,7 @@ let urlDB = mongoConfig.mongoURL;//'mongodb://' + mongoConfig.user + ':' + mongo
 const dbName = mongoConfig.dbName;
 let dbo;
 let fucksBase;
+let arr = []
 
 (function mongo_starter() {
     MongoClient.connect(urlDB, {   // + '/' + dbName, {
@@ -27,18 +28,7 @@ let fucksBase;
 //add/edit new record to game list
 async function addFucksUrls(body) {
     try {
-        for (let key in body) {
-            let recordExists = await checkRecordExistence(body[key].url)
-            if (recordExists) {
-                fucksBase.replaceOne({
-                    url: body[key].url
-                }, function (err, result){
-                    console.log('err: ', err, 'result: ', result)
-                });
-            } else {
-                await fucksBase.insertOne(body);
-            }
-        }
+        arr = body
     } catch (e) {
         console.log('MONGO_ERROR', e);
     }
@@ -46,16 +36,14 @@ async function addFucksUrls(body) {
 
 async function updateFuckUrl(body) {
     try {
-        return await fucksBase.updateOne(
-            { "url": body.url}, // Filter
-            { "isLive": body.isLive} // Update
-        )
-            .then((obj) => {
-                console.log('Updated - ' + obj);
-            })
-            .catch((err) => {
-                console.log('Error: ' + err);
-            })
+        const newArr = arr.map(obj => {
+            if (obj.id === 1) {
+                return {...obj, isLive: body.isLive};
+            }
+            return obj;
+        });
+
+        arr = newArr
     } catch (e) {
         console.log('MONGO_ERROR', e);
     }
@@ -63,21 +51,66 @@ async function updateFuckUrl(body) {
 
 async function getFuckUrls() {
     try {
-        return await fucksBase.find({'_id': -1}).toArray();
+        return arr
     } catch (e) {
         console.log('MONGO_ERROR', e);
     }
 }
 
-async function checkRecordExistence(url) {
-    try {
-        const dbRequest = await fucksBase.find({'url': url}).toArray();
-        console.log('dbRequest: ', dbRequest)
-        return Boolean(dbRequest.length);
-    } catch (e) {
-        console.log('MONGO_ERROR', e);
-    }
-}
+// //add/edit new record to game list
+// async function addFucksUrls(body) {
+//     try {
+//         for (let key in body) {
+//             let recordExists = await checkRecordExistence(body[key].url)
+//             if (recordExists) {
+//                 fucksBase.replaceOne({
+//                     url: body[key].url
+//                 }, function (err, result){
+//                     console.log('err: ', err, 'result: ', result)
+//                 });
+//             } else {
+//                 await fucksBase.insertOne(body);
+//             }
+//         }
+//     } catch (e) {
+//         console.log('MONGO_ERROR', e);
+//     }
+// }
+//
+// async function updateFuckUrl(body) {
+//     try {
+//         return await fucksBase.updateOne(
+//             { "url": body.url}, // Filter
+//             { "isLive": body.isLive} // Update
+//         )
+//             .then((obj) => {
+//                 console.log('Updated - ' + obj);
+//             })
+//             .catch((err) => {
+//                 console.log('Error: ' + err);
+//             })
+//     } catch (e) {
+//         console.log('MONGO_ERROR', e);
+//     }
+// }
+//
+// async function getFuckUrls() {
+//     try {
+//         return await fucksBase.find({'_id': -1}).toArray();
+//     } catch (e) {
+//         console.log('MONGO_ERROR', e);
+//     }
+// }
+//
+// async function checkRecordExistence(url) {
+//     try {
+//         const dbRequest = await fucksBase.find({'url': url}).toArray();
+//         console.log('dbRequest: ', dbRequest)
+//         return Boolean(dbRequest.length);
+//     } catch (e) {
+//         console.log('MONGO_ERROR', e);
+//     }
+// }
 
 
 module.exports.addFuckUrls = addFucksUrls
